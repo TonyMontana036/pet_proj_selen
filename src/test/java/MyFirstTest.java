013957233c5c708ad26cd7ecbef2a6fd11c57e8e
Anton S <mudaq@mail.ru>
2025-03-18 22:04:26 +0300

    Настройка и тест
    Добавил зависимости и первый тест

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,33 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MyFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firstTest() {
+        driver.get("http://localhost/litecart/admin/.");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+
+    @After
+    public void stop() {
+        driver.close();
+        driver = null;
+    }
+
+}
